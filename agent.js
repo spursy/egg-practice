8713684fcd51cd297b6b90904d9ec1307cfab5bd
yang yang <yangyang@yangdeMacBook-Pro.local>
2018-10-12 13:37:34 +0800

    add files

---
+++ agent.js
@@ -0,0 +1,11 @@
+const Subscriber = require('./lib/subscriber');
+
+module.exports = agent => {
+    agent.logger.info("init subscriber");
+    const subscriber = new Subscriber();
+    subscriber.on("changed", () => {
+        agent.messager.sendToApp('refresh', 'push');
+    })
+    subscriber.on("error", err => agent.logger.error(err));
+}
+
